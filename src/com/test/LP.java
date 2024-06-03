aca9ad19f2a4c192ad55dfe83b8fa66c3cbecb63
adgad111 <1643058734@qq.com>
2024-06-03 21:52:14 +0800

    修改

---
+++ src/com/test/LP.java
@@ -0,0 +1,43 @@
+package com.test;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class LP extends JLayeredPane implements MouseListener, MouseMotionListener {
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
