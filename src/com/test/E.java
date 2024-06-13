b8a3398c60b7d436a92f1d4ed8f60fcfc3802e05
adgad111 <1643058734@qq.com>
2024-06-13 20:02:10 +0800

    数据使用

--- src/com/test/E.java
+++ src/com/test/E.java
@@ -1,21 +1,42 @@
 package com.test;
 
-import com.sun.java.swing.plaf.windows.WindowsButtonListener;
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.sql.*;
 
-public class E{
+/*
+//数据库连接和基础查询
+public class E {
     public static void main(String[] args) {
-        new Hua_Rong_Dao();
-        String str=in.readLine();
-        byte e[]=str.getBytes("iso-8859-1");
-        String a=new String(e);
-        
+        Connection con=null;
+        String url = "jdbc:mysql://127.0.0.1:3306/javatest?user=root&password=&useSSL=true";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            System.out.println("驱动错误");
+        }
+        try {
+            con = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println("连接错误");
+        }
+      try {
+          Statement sql = con.createStatement();
+          ResultSet rs = sql.executeQuery("SELECT * FROM student ");
+          while (rs.next()) {
+              String id = rs.getString(1);
+              String name = rs.getString(2);
+              String sex = rs.getString(3);
+              String age = rs.getString(4);
+              System.out.printf("%s\t", id);
+              System.out.printf("%s\t", name);
+              System.out.printf("%s\t", sex);
+              System.out.printf("%s\t\n", age);
+          }
+          con.close();
+      }catch(SQLException ee){
+              System.out.println(ee);
+          }
+        }
     }
-}
+*/
